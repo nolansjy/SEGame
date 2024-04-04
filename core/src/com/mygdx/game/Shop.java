11163c00c4f3dcdf0b01d3b248953eda662e680a
Darwisy <hcyrr1@nottingham.edu.my>
2024-04-04 14:12:49 +0800

    Added shop features. -> Item appear grayscale -> Debug for add/sub quills

--- core/src/com/mygdx/game/Shop.java
+++ core/src/com/mygdx/game/Shop.java
@@ -2,31 +2,34 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
-import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
-import com.badlogic.gdx.scenes.scene2d.ui.Slider;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class Shop implements Screen {
     final SEMain game;
@@ -43,52 +46,118 @@ public class Shop implements Screen {
         camera = new OrthographicCamera();
         assetManager = game.getAssetManager();
         skin = assetManager.get("earthskin-ui/earthskin.json",Skin.class);
-        stage = new Stage(new FitViewport(450,854,camera));
+        stage = new Stage(new FitViewport(450,894,camera));
         Gdx.input.setInputProcessor(stage);
     }
 
     public void show() { //edit screen here
+        Color grayscale = new Color(0.2f,0.2f,0.2f,1f);
+        Color colorDefault = new Color(1f, 1f, 1f, 1f);
+
+        //TODO: Fix the alignment of the tables to match those in SEGameScreen.
+        //headUI initiate
+        Table headUI = new Table();
+        headUI.align(Align.topLeft).padLeft(15);
+        headUI.setFillParent(true);
+        stage.addActor(headUI);
+
+        //headUI elements
+        Image img = new Image(skin, "quill");
+        headUI.add(img).padRight(15);
+        Label wallet = new Label(String.valueOf(User.getQuills()), skin, "button");
+        headUI.add(wallet);
+
+
+        //menu initiate
         Table menu = new Table();
+        //menu.align(Align.center);
         menu.setFillParent(true);
         stage.addActor(menu);
 
-        Label decoLabel = new Label("Decorations", skin);
-        decoLabel.setFontScale(2f);
-        menu.add(decoLabel).row();
-
-
+        //decoration Table
+        Label decoLabel = new Label("Decorations", skin, "button");
+        menu.add(decoLabel).padBottom(15).row();
         Table decoTable = new Table();
-        //decoTable.space(15);
-        //Texture something = new Texture(Gdx.files.internal("fountain-test.png"));
-        //ImageButton dis1 = new ImageButton(something);
 
-        TextButton fir = new TextButton("Deco Item 1", skin);
-        TextButton sec = new TextButton("Deco Item 2", skin);
+        Image firImg = new Image(skin, "quill");
+        Image secImg = new Image(skin, "quill");
+        if (User.getQuills() < 50){firImg.setColor(grayscale);}
+        if (User.getQuills() < 80){secImg.setColor(grayscale);}
+        decoTable.add(firImg).size(150,150).padRight(15);
+        decoTable.add(secImg).size(150,150);
+        decoTable.row();
+
+        TextButton fir = new TextButton("50 quills", skin);
+        TextButton sec = new TextButton("80 quills", skin);
         decoTable.add(fir).width(150).padRight(15);
-        decoTable.add(sec).width(150).padRight(15);
+        decoTable.add(sec).width(150);
         menu.add(decoTable).row();
-        decoTable.pad(0,15,105,15);
+        decoTable.padBottom(105);
 
-        Label foodLabel = new Label("Bird feeds", skin);
-        foodLabel.setFontScale(2f);
+        //birdfeeder Table
+        Label foodLabel = new Label("Bird feeds", skin, "button");
         menu.add(foodLabel).row();
 
         Table foodTable = new Table();
-        //foodTable.space(15);
-        TextButton thi = new TextButton("Food Item 1", skin);
-        TextButton fou = new TextButton("Food Item 2", skin);
-        TextButton fif = new TextButton("Food Item 3", skin);
-        TextButton six = new TextButton("Food Item 4", skin);
+        TextButton thi = new TextButton("Fruit", skin);
+        TextButton fou = new TextButton("Seeds", skin);
+        TextButton fif = new TextButton("Sugar", skin);
+        TextButton six = new TextButton("Worms", skin);
         foodTable.add(thi).width(150).padRight(15);
-        foodTable.add(fou).width(150).padRight(15);
+        foodTable.add(fou).width(150);
+        foodTable.row().pad(15,0,15,0);
         foodTable.add(fif).width(150).padRight(15);
-        foodTable.add(six).width(150).padRight(15);
+        foodTable.add(six).width(150);
         menu.add(foodTable).row();
-        foodTable.pad(0,15,45,15);
+        foodTable.padBottom(45);
 
-        ScrollPane scrolls = new ScrollPane(foodTable);
-        scrolls.setScrollingDisabled(false, true);
-        menu.add(scrolls);
+
+        //This table won't appear in the final game. The purpose of this is to test
+        //the display of items based on the number of quills the user has collected
+        Table temp = new Table();
+        TextButton addQ = new TextButton("+10", skin);
+        TextButton subQ = new TextButton("-10", skin);
+        temp.add(addQ).padRight(15);
+        temp.add(subQ);
+        menu.add(temp).row();
+
+        addQ.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                User.addQuills(10);
+                wallet.setText(String.valueOf(User.getQuills()));
+
+                if (User.getQuills() >= 50){firImg.setColor(colorDefault);}
+                if (User.getQuills() >= 80){secImg.setColor(colorDefault);}
+                stage.act();
+                stage.draw();
+                return true;
+            }
+        });
+
+        subQ.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                User.addQuills(-10);
+                wallet.setText(String.valueOf(User.getQuills()));
+
+                if (User.getQuills() < 50){firImg.setColor(grayscale);}
+                if (User.getQuills() < 80){secImg.setColor(grayscale);}
+                stage.act();
+                stage.draw();
+                return true;
+            }
+        });
+
+        //returnButton
+        TextButton returnBtn = new TextButton("Return", skin);
+        stage.addActor(returnBtn);
+        returnBtn.addListener(new ChangeListener(){
+            @Override
+            public void changed(ChangeEvent event, Actor actor){
+                game.setScreen(new SEGameScreen(game));
+            }
+        });
     }
 
     @Override
